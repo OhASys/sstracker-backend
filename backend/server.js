aa4bed22c1e0645c6806be3cc6d1b13bca3dc07d
OhASys <orochiikof@gmail.com>
2025-07-27 13:05:47 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,46 +1,33 @@
-import express from "express";
-import http from "http";
-import { Server } from "socket.io";
-import mysql from "mysql2/promise";
-import cors from "cors";
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
-
 app.use(cors());
 app.use(express.json());
 
-// MySQL connection
-const db = await mysql.createConnection({
-  host: "sql103.infinityfree.com",
-  user: "if0_39531346",
-  password: "m08js19QJQoI",
-  database: "if0_39531346_sstracker"
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
 });
 
-// API route to check if backend is alive
-app.get("/", (req, res) => {
-  res.send("Sonic Shuriken Tracker backend is running");
-});
-
-// Socket.IO for real-time
 io.on("connection", (socket) => {
-  console.log("A user connected");
+    console.log("A user connected:", socket.id);
 
-  socket.on("join_user", (userId) => {
-    socket.join(`user_${userId}`);
-    console.log(`User ${userId} joined`);
-  });
+    socket.on("disconnect", () => {
+        console.log("A user disconnected:", socket.id);
+    });
+});
 
-  socket.on("update_tasks", (data) => {
-    io.to(`user_${data.userId}`).emit("tasks_updated", data);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("A user disconnected");
-  });
+app.get("/", (req, res) => {
+    res.send("Sonic Shuriken's Tracker backend is running!");
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
